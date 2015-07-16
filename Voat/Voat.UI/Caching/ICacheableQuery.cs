1a24db3a4c014ee33400c72d2ffd33138769b16f
Sean Grimes <sean@codemunkey.net>
2015-07-16 18:08:47 -0500

    Savepoint

--- Voat/Voat.UI/Caching/ICacheableQuery.cs
+++ Voat/Voat.UI/Caching/ICacheableQuery.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Voat.Caching
 {
-    interface ICacheableQuery<TQueryType, TReturnType> : ICacheable
+    public interface ICacheableQuery<TQueryType, TReturnType> : ICacheable
     {
         TReturnType Handle(TQueryType query);
     }
